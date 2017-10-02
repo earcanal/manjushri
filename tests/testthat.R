68af72908aeeed107976375a6f7fa27909ead1d6
Paul Sharpe <paul@miraclefish.com>
2017-10-02 16:23:32 +0100

    Add some tests.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(manjushri)
+
+test_check("manjushri")
---
+++ tests/testthat/test_breath_counting.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+context('Breath counting')
+
+# fixture setup
+bc_df       <- read_csv('../fixtures/bc.csv')
+bc_accuracy <- bc_df %>%
+  select(subject) %>%
+  unique() %>%
+  dplyr::rename(participant = subject) %>%
+  mutate(total=0,correct=0,incorrect=0)
+
+test_that("fixture is a data frame", {
+  expect_is(bc_df, "data.frame")
+})
+
+test_that("breath_counting_accuracy() returns a data frame", {
+  expect_is(breath_counting_accuracy(p = bc_df[, 'subject'], bc_df = bc_df), "data.frame")
+})
+
+test_that("breath_counting_accuracy() returns a data frame via do()", {
+  bc_accuracy <- expand.grid(p=bc_accuracy$participant) %>%
+    do(., breath_counting_accuracy(.$p, bc_df)) %>%
+    rowwise %>%
+    arrange(p)
+  expect_is(bc_accuracy, "data.frame")
+})
+
+
