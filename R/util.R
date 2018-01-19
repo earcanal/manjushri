4b779216bb06f0dac11e339444f073980470c512
Paul Sharpe <paul@miraclefish.com>
2018-01-19 16:10:02 +0000

    Remove misleading words!

--- R/util.R
+++ R/util.R
@@ -1,6 +1,5 @@
 #' Get participant information
 #'
-#' Summarise breath counting accuracy
 #' @export
 #' @param df Data frame with 'p' and 'condition' columns
 #' @param c String used to filter() 'condition' column
